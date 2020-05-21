19dfc43ab21d20ab470d5359c0cf0c0e3e549652
Dániel Buga <daniel@revolutionrobotics.org>
2020-05-21 13:05:38 +0200

    Improve crate-level description

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
-//! This crate provides a heapless slab allocator.
+//! This crate provides a heapless, fixed size, unordered data structure, inspired by SlotMap.
 //!
-//! Slots implements a "heapless", fixed size, unordered data structure,
-//! inspired by SlotMap.
+//! The following basic operations (all of them `O(1)`) are defined for Slots:
+//! - Insert: store data and retrieve a handle for later access
+//! - Read, modify: use the given handle to access the data without removal
+//! - Take: use the given handle to remove data
 //!
 //! There are two variations of this data structure:
 //!  * [`Slots`](./slots/index.html), where elements can only be modified using a `Key` that can't be copied
-//!  * [`UnrestrictedSlots`](../unrestricted/index.html), where elements are free to be modified by anyone
+//!  * [`UnrestrictedSlots`](./unrestricted/index.html), where elements are free to be modified by anyone
 
 #![cfg_attr(not(test), no_std)]
 
