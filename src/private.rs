1cd6868cb90d7833aa3c09f51fc31fa67e148ff1
Dániel Buga <bugadani@gmail.com>
2022-03-25 11:05:25 +0100

    Fix hidden attribute

--- src/private.rs
+++ src/private.rs
@@ -1,7 +1,7 @@
 //! **Ignore me!** This file contains implementation details
 //! that are conceptually private but must be technically public.
 
-#[doc(hide)]
+#[doc(hidden)]
 pub enum Entry<IT> {
     Used(IT),
     EmptyNext(usize),
